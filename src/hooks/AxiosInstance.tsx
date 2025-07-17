b6ed93c314a86c20cd4ca5e6f70e08b78f125f40
srikrishnameet <srikrishna@meetowner.in>
2025-07-17 09:30:59 +0530

    added the latest changes

--- src/hooks/AxiosInstance.tsx
+++ src/hooks/AxiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const ngrokAxiosInstance: AxiosInstance = axios.create({
-  baseURL: 'https://7ac2fd315b14.ngrok-free.app',
+  baseURL: 'https://ff6e8743d76f.ngrok-free.app',
 
   headers: {
     'Content-Type': 'application/json',
