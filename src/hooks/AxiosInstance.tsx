5837785ce22ddc2f4a0a96a38e09b1390a58e11a
rishitharaghana <jrishitha2001@gmail.com>
2025-07-23 09:58:07 +0530

    sd

--- src/hooks/AxiosInstance.tsx
+++ src/hooks/AxiosInstance.tsx
@@ -1,9 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const ngrokAxiosInstance: AxiosInstance = axios.create({
-  // baseURL: 'https://72189bcc9357.ngrok-free.app',
-  baseURL: "https://crmapi.mntechs.com",
-
+  // baseURL: "https://crmapi.mntechs.com",
+ baseURL: 'http://localhost:3002',
   headers: {
     "Content-Type": "application/json",
   },
