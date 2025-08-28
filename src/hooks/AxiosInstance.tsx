f4ef0b1a31908f5ae64673c0821c62c1606814a0
muthyala10 <muthylanayudu@meetowner.in>
2025-08-28 18:21:15 +0530

    asd

--- src/hooks/AxiosInstance.tsx
+++ src/hooks/AxiosInstance.tsx
@@ -3,7 +3,6 @@ import axios, { AxiosInstance } from "axios";
 const ngrokAxiosInstance: AxiosInstance = axios.create({
    baseURL: "https://crmapi.mntechs.com",
     //  baseURL: 'http://localhost:3002',
-
   headers: {
     "Content-Type": "application/json",
   },
